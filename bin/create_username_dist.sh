fe64aa990bff57e36611f75d8d89f0cae460d5b7
Erik Rauer <eriktr22@gmail.com>
2021-09-14 11:56:37 -0500

    Fix create_username_dist.sh
    A couple of small changes, such as fixing the shebang and correcting the name of the array used to store username info.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 wrkDir=$1
 tmpDir=$(mktemp -d)
@@ -9,6 +9,6 @@ cat $wrkDir/*/failed_login_data.txt | awk 'match($0, /[a-zA-z]+ [0-9]+ [0-9]+ ([
 
 touch $tmpDir/countNames.txt
 
-sort $tmpDir/usernames.txt | uniq -c | awk 'match($0, /([0-9]+) ([a-zA-Z0-9]+)/, group) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' >> $tmpDir/countNames.txt
+sort $tmpDir/usernames.txt | uniq -c | awk 'match($0, /([0-9]+) ([a-zA-Z0-9]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' >> $tmpDir/countNames.txt
 
 bin/wrap_contents.sh $tmpDir/countNames.txt html_components/username_dist $wrkDir/username_dist.html
