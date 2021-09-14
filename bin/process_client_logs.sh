e050db48f71a38d7b88e2dcb53b10284fbeb3182
Erik Rauer <eriktr22@gmail.com>
2021-09-14 10:33:21 -0500

    Fix regex and change expected path of the log files in process_client_logs.sh
    Fixes regex to capture hour of recorded times in logs and to capture double spaces when the day of the month is a single digit.
    Also changes process_client_logs.sh to look in ./var/log for the log files, which, based on the tests, appears to be the expected behavior.
    
    Co-Authored by: @day00090 Kyle Day

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -5,4 +5,4 @@ wrkDir=$1 #This is the working directory.
 cd "$wrkDir"
 
 touch failed_login_data.txt
-cat * | awk 'match($0, /([^\x58]+).* Failed password for (invalid user)? ([a-zA-Z0-9]+) from ([0-9\.]+)/, groups) {print groups[1] " " groups[3] " " groups[4] "\n"}' >> failed_login_data.txt
+cat var/log/* | awk 'match($0, /([a-zA-z]+)[[:space:]]{1,2}([0-9]+[[:space:]][0-9]+).* Failed password for (invalid user )?([a-zA-Z0-9]+) from ([0-9\.]+)/, groups) {print groups[1] " " groups[2] " " groups[4] " " groups[5] }' >> failed_login_data.txt
