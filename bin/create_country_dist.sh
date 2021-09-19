321ec23a28df0ef91b3e89cad6bea5c030d4182b
Kyle Day <day00090@macross.morris.umn.edu>
2021-09-19 13:53:05 -0500

    Fix create_country_dist.sh
    This commit fixes create_country_dist.sh by introducing a few sorting calls that sort the IP addresses as they are piped to different commands. This allowed for a simplification of the last awk statement, since we sort the IP addresses before they get there.
    
    Co-authored-by: Erik Rauer rauer007@morris.umn.edu

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -9,6 +9,6 @@ cat "$wrkDir"/*/failed_login_data.txt | awk 'match($0, /[a-zA-z]+ [0-9]+ [0-9]+ 
 
 touch "$tmpDir"/countCountries.txt
 
-sort "$tmpDir"/ip_addresses.txt | join | uniq -c -f 7 | awk 'match($0, /([0-9]+) [0-9\.]+ ([a-zA-Z]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' >> "$tmpDir"/countCountries.txt
+join <(sort "$tmpDir"/ip_addresses.txt) <(sort ./etc/country_IP_map.txt) | awk '{print $2;}' | sort | uniq -c | awk 'match($0, /([0-9]+) ([a-zA-Z]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' >> "$tmpDir"/countCountries.txt
 
 bin/wrap_contents.sh "$tmpDir"/countCountries.txt html_components/country_dist "$wrkDir"/country_dist.html
