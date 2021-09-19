3e1d10981e0432e8950a64b7b82899b0941f22e5
Kyle Day <day00090@macross.morris.umn.edu>
2021-09-19 15:25:10 -0500

    Fix Regex in process_client_logs.sh
    This fixes the regex in process_client_logs.sh to allow for the regex to recognizes dashes and underscores in usernames.
    
    Co-authored-by: Erik Rauer rauer007@morris.umn.edu

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,4 +6,4 @@ wrkDir=$1
 cd "$wrkDir" || exit
 
 touch failed_login_data.txt
-cat var/log/* | awk 'match($0, /([a-zA-z]+)[[:space:]]{1,2}([0-9]+[[:space:]][0-9]+).* Failed password for (invalid user )?([a-zA-Z0-9]+) from ([0-9\.]+)/, groups) {print groups[1] " " groups[2] " " groups[4] " " groups[5] }' >> failed_login_data.txt
+cat var/log/* | awk 'match($0, /([a-zA-z]+)[[:space:]]{1,2}([0-9]+[[:space:]][0-9]+).* Failed password for (invalid user )?([\-\_a-zA-Z0-9]+) from ([0-9\.]+)/, groups) {print groups[1] " " groups[2] " " groups[4] " " groups[5] }' >> failed_login_data.txt
