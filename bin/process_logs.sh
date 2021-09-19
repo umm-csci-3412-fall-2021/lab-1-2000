1bbf969bc5199cd95ba8b2a52de113151d96dbaa
Kyle Day <day00090@macross.morris.umn.edu>
2021-09-19 15:28:55 -0500

    Fix Shebang and Add Quotation Marks in process_logs.sh
    This commit fixes the shebang by repositioning the exclamation point. In addition, it also adds quotation marks around all instances of $file, since $file is technically a variable.
    
    Co-authored-by: Erik Rauer rauer007@morris.umn.edu

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,11 +1,11 @@
-#/!bin/bash
+#!/bin/bash
 
 tmpDir=$(mktemp -d)
 
 for file in "$@"; do
-	base=$(basename $file _secure.tgz)
+	base=$(basename "$file" _secure.tgz)
 	mkdir "$tmpDir"/"$base"
-	tar -xzf $file -C "$tmpDir"/"$base"
+	tar -xzf "$file" -C "$tmpDir"/"$base"
 	bin/process_client_logs.sh  "$tmpDir"/"$base"	
 done
 
